7e4f470bbc517affea459db25e7ee67c8a27d68c
Nick Loadholtes <nick@ironboundsoftware.com>
2018-11-14 15:01:41 -0500

    Generate the right type of app

--- README.md
+++ README.md
@@ -1,12 +1,36 @@
 # chatty-bird
 
-A Clojure library designed to ... well, that part is up to you.
+FIXME: description
+
+## Installation
+
+Download from http://example.com/FIXME.
 
 ## Usage
 
-FIXME
+FIXME: explanation
+
+    $ java -jar chatty-bird-0.1.0-standalone.jar [args]
+
+## Options
+
+FIXME: listing of options this app accepts.
+
+## Examples
+
+...
+
+### Bugs
+
+...
+
+### Any Other Sections
+### That You Think
+### Might be Useful
 
 ## License
+
+MIT 
 
 Copyright © 2018 FIXME
 
--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject chatty-bird "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main ^:skip-aot chatty-bird.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
