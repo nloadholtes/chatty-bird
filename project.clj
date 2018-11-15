7ee5fd3514f2ea995ca8e25aeba8efbb5672c192
Nick Loadholtes <nick@ironboundsoftware.com>
2018-11-14 20:53:13 -0500

    Pulling in twitter

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [twitter-api "1.8.0"]]
   :main ^:skip-aot chatty-bird.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/chatty_bird/core.clj
+++ src/chatty_bird/core.clj
@@ -1,5 +1,10 @@
 (ns chatty-bird.core
-  (:gen-class))
+  (:gen-class)
+  (:use [twitter.oauth]
+        [twitter.callbacks]
+        [twitter.callbacks.handlers]
+        [twitter.api.restful])
+  (:import [twitter.callbacks.protocols SyncSingleCallback]))
 
 (defn -main
   "I don't do a whole lot ... yet."
