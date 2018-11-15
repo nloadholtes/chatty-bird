d5f0d695ef189f506b443a117ea2e7c6aa7a4a4a
Nick Loadholtes <nick@ironboundsoftware.com>
2018-11-14 21:11:23 -0500

    Let's use environment vars

--- src/chatty_bird/core.clj
+++ src/chatty_bird/core.clj
@@ -6,7 +6,17 @@
         [twitter.api.restful])
   (:import [twitter.callbacks.protocols SyncSingleCallback]))
 
+
+(def my-creds (make-oauth-creds (System/getenv "CONSUMER_KEY")
+                                (System/getenv "CONSUMER_SECRET")
+                                (System/getenv "ACCESS_TOKEN")
+                                (System/getenv "ACCESS_TOKEN_SECRET")))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println my-creds))
+
+
