9ee3d3e871035569acdd62d92f4516e0e2ca6b59
Nick Loadholtes <nick@ironboundsoftware.com>
2018-12-08 15:51:52 -0500

    Fixing wait time env issues

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ docker run -it --rm -e CONSUMER_KEY="$CONSUMER_KEY" \
        -e CONSUMER_SECRET="$CONSUMER_SECRET" \
        -e ACCESS_TOKEN="$ACCESS_TOKEN" \
        -e ACCESS_TOKEN_SECRET="$ACCESS_TOKEN_SECRET" \
-       -e WAIT_TIME="$3" \
+       -e WAIT_TIME="$WAIT_TIME" \
 	-v $PWD:/data \
        chatty $2
